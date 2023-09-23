df1098a44c3f3c22160676cdf4a08b46cc1f0c7a
JoseACd744 <u20211d744@upc.edu.pe>
2023-09-23 17:17:37 -0500

    feature: log-in and sign-up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component'; // Add this
 import { CoursesComponent } from './components/courses/courses.component';
+import  LogInComponent  from './components/log-in/log-in.component'; // Add this
+import  SignUpComponent  from './components/sign-up/sign-up.component'; // Add this
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
+  { path: 'home', component: HomeComponent},
   { path: 'courses', component: CoursesComponent },
+  { path: 'login', component: LogInComponent }, 
+  { path: 'signup', component: SignUpComponent },
   { path: '', pathMatch: 'full', redirectTo: 'home'},
-  { path: '**', pathMatch: 'full', redirectTo: 'home'}
+  { path: '**', pathMatch: 'full', redirectTo: 'home'},
+ 
 ];
 
 @NgModule({
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -12,5 +14,16 @@ import { MatDividerModule } from '@angular/material/divider';
   imports: [MatButtonModule, MatCardModule, MatDividerModule, MatIconModule]
 })
 export class HomeComponent {
+  private _router = inject(Router);
 
+  private authservice = inject(AuthService);
+
+  async logOut(): Promise<void> {
+    try {
+      await this.authservice.logOut();
+      this._router.navigateByUrl('/auth/log-in');
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
