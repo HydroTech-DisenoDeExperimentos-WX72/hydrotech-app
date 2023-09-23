cac4209fda53c6f1a29cca1668bef7b49c603f7f
Franco Yance <frank_2605yagu@hotmail.com>
2023-09-23 18:28:18 -0500

    feature:Articles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { HomeComponent } from './components/home/home.component'; // Add this
 import { CoursesComponent } from './components/courses/courses.component';
 import  LogInComponent  from './components/log-in/log-in.component'; // Add this
 import  SignUpComponent  from './components/sign-up/sign-up.component'; // Add this
-
+import { ArticlesComponent } from './components/articles/articles.component';
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
   { path: 'courses', component: CoursesComponent },
   { path: 'login', component: LogInComponent }, 
   { path: 'signup', component: SignUpComponent },
+  { path: 'articles', component: ArticlesComponent },
   { path: '', pathMatch: 'full', redirectTo: 'home'},
   { path: '**', pathMatch: 'full', redirectTo: 'home'},
  
