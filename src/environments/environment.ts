db4fd36da0a3d004b5fed2db30f5624b4082ae3f
JoseACd744 <142860250+JoseACd744@users.noreply.github.com>
2023-10-18 15:55:34 -0500

    chore: firebase deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,10 @@
 export const environment = {
   firebase: {
-    projectId: 'angular-auth-opsource',
-    appId: '1:937225638162:web:0f322e48242c8a7a06a3cb',
-    storageBucket: 'angular-auth-opsource.appspot.com',
-    apiKey: 'AIzaSyBikZNdrzDEry1coAIKih8QcTjEfmBMmAM',
-    authDomain: 'angular-auth-opsource.firebaseapp.com',
-    messagingSenderId: '937225638162',
-    measurementId: 'G-6RV4ZQ83X4',
+    projectId: 'deploy-greengrow',
+    appId: '1:830150306191:web:c7dd08aa36c81be85ec3fa',
+    storageBucket: 'deploy-greengrow.appspot.com',
+    apiKey: 'AIzaSyDqcqyE-hsgiGxWguBFDK9wDG0xY_wrRIk',
+    authDomain: 'deploy-greengrow.firebaseapp.com',
+    messagingSenderId: '830150306191',
   },
 };
