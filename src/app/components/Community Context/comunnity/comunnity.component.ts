2a96b5f023b093d3ef710dd0729d7aa90e5fae8f
Arnol Cáceres <116393375+arnolcb@users.noreply.github.com>
2023-11-18 20:19:49 -0500

    fix: trends display

--- src/app/components/Community Context/comunnity/comunnity.component.ts
+++ src/app/components/Community Context/comunnity/comunnity.component.ts
@@ -8,7 +8,7 @@ import { DbService } from 'src/app/core/services/db.service';
 })
 export class ComunnityComponent {
   posts: any[] = [];
-  trends: string[] = [];
+  trends: any[] = [];
 
   constructor(private dbService: DbService) {}
 
